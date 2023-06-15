8935bab96a0228d02aceba75504dd5d12eed8fe8
AbhiAdhav007 <120732315+AbhiAdhav007@users.noreply.github.com>
2023-06-15 20:13:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function decimalToBinary(num) {
-	let temp = "";
-	while(num != 0){
-		temp += num % 2
-		num = Math.floor(num / 2)
-	}
-	return Number(temp.split("").reverse().join(""));
+function threeSum(arr, target) {
+// write your code here function convertToBinary(x) {
+    let bin = 0;
+    let rem, i = 1, step = 1;
+    while (x != 0) {
+        rem = x % 2;
+        console.log(
+            `Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
+        );
+        x = parseInt(x / 2);
+        bin = bin + rem * i;
+        i = i * 10;
+    }
+    console.log(`Binary: ${bin}`);
 }
-module.exports = decimalToBinary;
+  
+}
+
+module.exports = threeSum;
